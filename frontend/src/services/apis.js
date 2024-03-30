bad8cc1ee07c1838b6df8b162e4399b8a54ddaf6
Ashish Soni <4840asoni@gmail.com>
2024-03-30 18:16:55 +0530

    add api

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:8800/api";
+const SOCIAL_APP_API = "https://social-app-i0jz.onrender.com/";
+// const BASE_URL = "http://localhost:8800/api";
+const BASE_URL = SOCIAL_APP_API +"api";  // for production use
 
 // AUTH ENDPOINTS
 export const endpoints = {
